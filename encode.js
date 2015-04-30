50311e78f2908d0577a8c607a0fd0350af51eb0f
Katelyn Gadd <kg@luminance.org>
2015-04-30 15:40:55 -0700

    Optionally dump the expected JS alongside the expected AST

--- encode.js
+++ encode.js
@@ -5,22 +5,25 @@ require('./node-augh.js');
 require('./Upstream/encoding/encoding.js');
 
 var esprima = require('./Upstream/esprima/');
+var escodegen = require('./Upstream/escodegen/escodegen.browser.js').escodegen;
 var astEncoder = require('./ast-encoder.js');
 var fs = require('fs');
 
 if (process.argv.length < 4) {
-  console.log("USAGE: encode input.js output.webasm [astOutput.json]");
+  console.log("USAGE: encode input.js output.webasm [astOutput.json] [expectedOutput.js]");
   process.exit(1);
 }
 
 var inputFile = process.argv[2];
 var outputFile = process.argv[3];
 var outputAstFile = process.argv[4];
+var expectedOutputJsFile = process.argv[5];
 
 var inputJs = fs.readFileSync(inputFile, { encoding: "utf8" });
 var fdOut = fs.openSync(outputFile, "w");
 
 var inputAst = esprima.parse(inputJs);
+
 var outputModule = astEncoder.astToModule(inputAst);
 
 var segments = astEncoder.serializeModule(outputModule);
@@ -34,3 +37,6 @@ fs.closeSync(fdOut);
 
 if (outputAstFile)
   fs.writeFileSync(outputAstFile, JSON.stringify(inputAst));
+
+if (expectedOutputJsFile)
+  fs.writeFileSync(expectedOutputJsFile, escodegen.generate(inputAst));
--- test-roundtrip.bat
+++ test-roundtrip.bat
@@ -1,2 +1,2 @@
-node encode.js ast-encoder.js Test/ast-encoder.webasm Test/ast-encoder.json
+node encode.js ast-encoder.js Test/ast-encoder.webasm Test/ast-encoder.json Test/ast-encoder.js.expected
 node decode.js Test/ast-encoder.webasm Test/ast-encoder.js.out
